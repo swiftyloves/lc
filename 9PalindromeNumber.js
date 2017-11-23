aa58437893d51898fe2687c4a0279ac9ccc87b88
Jie-Wei <swiftywu@yahoo.com>
2017-11-22 20:30:19 -0500

    9. Palindrome Number -- modified

--- 9PalindromeNumber.js
+++ 9PalindromeNumber.js
@@ -5,17 +5,19 @@
  */
 var isPalindrome = function(x) {
     if (x < 0) {return false;}
+    if (x == 0) {return true;}
+
+    let len = 0;
     let list = [];
-    let len = x.toString().length;
-    
-    while(len > 0) {
-        list.push(Math.floor (x / Math.pow(10, len - 1)));
-        x = x % Math.pow(10, len - 1);
-        len -= 1; 
+    while (x !== 0) {
+        len += 1;
+        list.push(x % 10);
+        x = Math.floor(x / 10);
+    }
+    let len_half = Math.floor(len / 2);
+    for (let i = 0; i < len_half; i++) {
+        if (list[i] !== list[len -  i - 1]) { return false; }
     }
 
-    for (let i = 0; i < Math.floor(list.length / 2); i++) {
-        if (list[i] !== list[list.length -  i - 1]) { return false;}
-    }
-    return true;
+    return true;    
 };
