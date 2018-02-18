ce769bd65ff1d7c124f4246e84f6da9aea2f6ae0
Jie-Wei <swiftywu@yahoo.com>
2018-02-17 22:20:41 -0500

    144. Binary Tree Preorder Traversal -- iteration

--- 144BinaryTreePreorderTraversal.js
+++ 144BinaryTreePreorderTraversal.js
@@ -1,3 +1,20 @@
+// No need to decompression since push self first ...
+var preorderTraversal = function(root) {
+    let node = root;
+    let sol = [];
+    let stack = [];
+    stack.push(root);
+    while (stack.length > 0) {
+        let node = stack.pop();
+        if (node === null) { continue; }
+        sol.push(node.val);
+        stack.push(node.right);
+        stack.push(node.left);
+    }
+    return sol;
+};
+
+// Recursion
 let preorder = function(node, arr) {
     if (node === null) { return; }
     arr.push(node.val);
