1a6b156d27162ff4f93e9b2efc6ec7ed003478f3
Jie-Wei <swiftywu@yahoo.com>
2018-02-18 08:59:45 -0500

    279. Perfect Squares -- Dp in iteration

--- 279PerfectSquares.js
+++ 279PerfectSquares.js
@@ -2,6 +2,26 @@
  * @param {number} n
  * @return {number}
  */
+
+/* dp in iteration *
+/* opt (n) =  min {opt (n - k * k)   s.t. 1 <= k && k * k <= n}.
+   opt (0) =  0
+*/
+var numSquares = function(n) {
+    let dp = [];
+    for(let i = 0; i <= n; i++) {
+        dp.push(Number.POSITIVE_INFINITY);
+    }
+    dp[0] = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let k = 1; k * k <= n, i - k * k >= 0; k++) {
+            dp[i] = Math.min(dp[i], dp[i - k * k] + 1);
+        }
+    }
+    return dp[n];
+};
+
+/* dp in recursion */
 let addSquares = function(target, arr) {
     if (arr[target] !== null) {
         return arr[target];
